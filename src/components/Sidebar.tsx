c9204fc96137bf5241d925a9f827a4779702c289
Patrick Mahloy <patrick.mahloy@gmail.com>
2023-05-25 20:46:35 -0400

    created sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,58 @@
+// import React from 'react';
+import { FaFire, FaGlasses, FaHandHoldingHeart, FaHome } from 'react-icons/fa';
+import { BsFillLightningChargeFill } from 'react-icons/bs';
+import { IoSettingsSharp } from 'react-icons/io5';
+
+import styles from '../styles/Sidebar.module.scss';
+
+const menu = [
+	{
+		icon: <FaHome />,
+		text: 'Home',
+	},
+	{
+		icon: <FaFire />,
+		text: 'Trending',
+	},
+	{
+		icon: <BsFillLightningChargeFill />,
+		text: 'Live',
+	},
+	{
+		icon: <FaHandHoldingHeart />,
+		text: 'Recommended',
+	},
+	{
+		icon: <FaGlasses />,
+		text: 'Watch List',
+	},
+	{
+		icon: <IoSettingsSharp />,
+		text: 'Settings',
+	},
+];
+
+export default function Sidebar() {
+	return (
+		<div className={styles.Sidebar}>
+			<div className={styles.top}>
+				<div className={styles.profile}>
+					<div className={styles.profile_avatar}>JD</div>
+					<div className={styles.profile_username}>Jane Doe</div>
+				</div>
+				<a href="#" className={styles.switch}>
+					Switch Profile
+				</a>
+			</div>
+			<div className={styles.menu}>
+				{menu.map((item, index) => (
+					<a key={index} className={styles.menu_link}>
+						{item.icon}
+						<span className={styles.menu_link_text}>{item.text}</span>
+					</a>
+				))}
+			</div>
+			<span className={styles.logo}>Ethereal</span>
+		</div>
+	);
+}
